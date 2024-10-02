cb46b84cd620ce4fcb06d54372da000958d98498
Wick <vic_the_gunners@hotmail.com>
2024-10-02 17:17:20 +0700

    postgres

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker, Session
 import os
 
 # Database setup
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@localhost:5432/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://postgres.eyfycqdqpslpnpecmoet:n75tjdrbgajdfgq3ogzdfn@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres')
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
